bbb7a4f3f7b79bde90d22a6aea0fa354a8b7a988
Stella McEwen <mcewen.stella@gmail.com>
2019-06-21 13:59:54 +0000

    .

--- lib/dog_breeds/country_of_origin.rb
+++ lib/dog_breeds/country_of_origin.rb
@@ -15,9 +15,8 @@ class DogBreeds::CountryOfOrigin
   end
   
   def self.add_dog_breed(dog_breed)
-    new_dog_breed = DogBreeds::DogBreed.new(dog_breed)
-    @breeds << new_dog_breed
-    new_dog_breed.country_of_origin = self
+    @breeds << dog_breed
+    dog_breed.country_of_origin = self
     dog_breed
   end
   
